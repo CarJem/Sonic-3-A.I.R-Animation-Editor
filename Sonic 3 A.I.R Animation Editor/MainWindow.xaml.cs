60dcbf50653fb508384a11abe7d72361f2ec49be
CarJem Generations <Cwallacecs@gmail.com>
2019-08-29 13:43:54 -0400

    - Implemented the Ability to Add and Remove Frames - Implemented Support for Centering Frames - Implemented Save As Support - Implemented the Exit Menu Item - Improved the Updating-Ability of the Editor Controls

--- Sonic 3 A.I.R Animation Editor/MainWindow.xaml.cs
+++ Sonic 3 A.I.R Animation Editor/MainWindow.xaml.cs
@@ -44,10 +44,10 @@ namespace Sonic_3_A.I.R_Animation_Editor
         {
             bool isAnimationLoaded = CurrentAnimation != null;
             OpenMenuItem.IsEnabled = true;
-            SaveAsMenuItem.IsEnabled = isAnimationLoaded && false;
+            SaveAsMenuItem.IsEnabled = isAnimationLoaded;
             SaveMenuItem.IsEnabled = isAnimationLoaded;
             NewMenuItem.IsEnabled = false;
-            ExitMenuItem.IsEnabled = false;
+            ExitMenuItem.IsEnabled = true;
 
             if (isAnimationLoaded)
             {
@@ -74,19 +74,37 @@ namespace Sonic_3_A.I.R_Animation_Editor
             }
         }
 
-        private void SaveFileAs(object sender, RoutedEventArgs e)
+        private void SaveFileAsEvent(object sender, RoutedEventArgs e)
         {
-
+            FileSaveAs();
         }
 
-        private void SaveFile(object sender, RoutedEventArgs e)
+        private void FileSaveAs()
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog()
+            {
+                Filter = "JSON Files (*.json) | *.json"
+            };
+            if (fileDialog.ShowDialog() == true)
+            {
+                CurrentAnimation.Save(fileDialog.FileName);
+            }
+        }
+
+        private void FileSave()
         {
             CurrentAnimation.Save();
         }
 
+        private void SaveFileEvent(object sender, RoutedEventArgs e)
+        {
+            if (CurrentAnimation.FileLocation == "") FileSaveAs();
+            else FileSave();
+        }
+
         private void ExitEditor(object sender, RoutedEventArgs e)
         {
-
+            Environment.Exit(0);
         }
 
         #endregion
@@ -130,12 +148,31 @@ namespace Sonic_3_A.I.R_Animation_Editor
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (CurrentAnimation != null)
+            {
+                if (CurrentFrame != null && EntriesList.SelectedItem != null)
+                {
+                    int index = EntriesList.SelectedIndex;
+                    CurrentAnimation.FrameList.Insert(index, new Sonic3AIRAnim.Sonic3AIRFrame());
+                    UpdateUI();
+                }
+            }
         }
 
         private void RemoveButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (CurrentAnimation != null)
+            {
+                if (CurrentFrame != null && EntriesList.SelectedItem != null)
+                {
+                    int index = EntriesList.SelectedIndex;
+                    if (MessageBox.Show($"Are you sure you want to remove \"{CurrentAnimation.FrameList[index].Name}\"?", "Remove Frame", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                    {
+                        CurrentAnimation.FrameList.RemoveAt(index);
+                        UpdateUI();
+                    }
+                }
+            }
         }
 
 
@@ -157,6 +194,8 @@ namespace Sonic_3_A.I.R_Animation_Editor
                     int y = 0;
                     int width = 0;
                     int height = 0;
+                    int centerX = 0;
+                    int centerY = 0;
 
                     if (NewFrame)
                     {
@@ -172,11 +211,16 @@ namespace Sonic_3_A.I.R_Animation_Editor
                         CurrentFrame.Width = width;
                         CurrentFrame.Height = height;
 
+                        centerX = CurrentFrame.CenterX;
+                        centerY = CurrentFrame.CenterY;
+
                         AllowUpdate = false;
                         YNUD.Value = y;
                         XNUD.Value = x;
                         WidthNUD.Value = width;
                         HeightNUD.Value = height;
+                        CenterXNUD.Value = centerX;
+                        CenterYNUD.Value = centerY;
                         FileTextBox.Text = CurrentFrame.File;
                         NameTextBox.Text = CurrentFrame.Name;
                         AllowUpdate = true;
@@ -189,11 +233,16 @@ namespace Sonic_3_A.I.R_Animation_Editor
                         y = (int)area.Y;
                         width = (int)area.Width;
                         height = (int)area.Height;
+                        centerX = (int)CenterXNUD.Value;
+                        centerY = (int)CenterYNUD.Value;
 
                         CurrentFrame.X = x;
                         CurrentFrame.Y = y;
                         CurrentFrame.Width = width;
                         CurrentFrame.Height = height;
+
+                        CurrentFrame.CenterX = centerX;
+                        CurrentFrame.CenterY = centerY;
 
                     }
 
@@ -215,8 +264,8 @@ namespace Sonic_3_A.I.R_Animation_Editor
                         ImageScale.ScaleY = Zoom;
                         CanvasImage.RenderTransformOrigin = new Point(0, 0);
 
-                        double left = GetLeft(CanvasView.ActualWidth, 0, -(int)(CurrentFrame.Width / 2), (int)CurrentFrame.Width, Zoom);
-                        double top = GetTop(CanvasView.ActualHeight, 0, -(int)(CurrentFrame.Height / 2), (int)CurrentFrame.Height, Zoom);
+                        double left = GetLeft(CanvasView.ActualWidth, 0, -(int)(CurrentFrame.CenterX), (int)CurrentFrame.Width, Zoom);
+                        double top = GetTop(CanvasView.ActualHeight, 0, -(int)(CurrentFrame.CenterY), (int)CurrentFrame.Height, Zoom);
                         //double right = GetRight(left, (int)WidthNUD.Value, Zoom);
                         //double bottom = GetBottom(top, (int)HeightNUD.Value, Zoom);
 
@@ -229,27 +278,58 @@ namespace Sonic_3_A.I.R_Animation_Editor
                     else
                     {
                         CanvasImage.Source = null;
-
-                        YNUD.MaxValue = 0;
-                        XNUD.MaxValue = 0;
-                        WidthNUD.MaxValue = 0;
-                        HeightNUD.MaxValue = 0;
-
-                        YNUD.Value = 0;
-                        XNUD.Value = 0;
-                        WidthNUD.Value = 0;
-                        HeightNUD.Value = 0;
                     }
                 }
-                else CanvasImage.Source = null;
+                else
+                {
+                    AllowUpdate = false;
+                    CanvasImage.Source = null;
+                    NameTextBox.Text = CurrentFrame.Name;
+                    FileTextBox.Text = CurrentFrame.File;
+
+                    YNUD.MaxValue = CurrentFrame.Y;
+                    XNUD.MaxValue = CurrentFrame.X;
+                    WidthNUD.MaxValue = CurrentFrame.Width;
+                    HeightNUD.MaxValue = CurrentFrame.Height;
+
+                    YNUD.Value = CurrentFrame.Y;
+                    XNUD.Value = CurrentFrame.X;
+                    WidthNUD.Value = CurrentFrame.Width;
+                    HeightNUD.Value = CurrentFrame.Height;
+
+                    CenterXNUD.Value = CurrentFrame.CenterX;
+                    CenterYNUD.Value = CurrentFrame.CenterY;
+                    AllowUpdate = true;
+                }
+
+
+
+
             }
             else
             {
+                AllowUpdate = false;
                 CanvasImage.Source = null;
+                NameTextBox.Text = "";
+                FileTextBox.Text = "";
+
+                YNUD.MaxValue = 0;
+                XNUD.MaxValue = 0;
+                WidthNUD.MaxValue = 0;
+                HeightNUD.MaxValue = 0;
+
+                YNUD.Value = 0;
+                XNUD.Value = 0;
+                WidthNUD.Value = 0;
+                HeightNUD.Value = 0;
+
+                CenterXNUD.Value = 0;
+                CenterYNUD.Value = 0;
+                AllowUpdate = true;
             }
 
-
         }
+
 
         private void UpdateFrameImage()
         {
@@ -411,11 +491,15 @@ namespace Sonic_3_A.I.R_Animation_Editor
                 FileLocation = file.FullName;
                 string data = File.ReadAllText(file.FullName);
                 JToken stuff = JRaw.Parse(data);
-                foreach (JToken child in stuff.Children())
+
+                foreach (var child in stuff.Children())
                 {
                     string _name = "";
                     string _file = "";
                     Rect _rect = new Rect();
+                    int? _center_x = null;
+                    int? _center_y = null;
+
                     if (child.HasValues)
                     {
                         _name = child.Path;
@@ -436,14 +520,33 @@ namespace Sonic_3_A.I.R_Animation_Editor
                                     }
                                     _rect = new Rect(Rect[0], Rect[1], Rect[2], Rect[3]);
                                 }
+                                else if (content.Name == "Center")
+                                {
+                                    List<int> Center = new List<int>();
+                                    foreach (string item in content.Value.ToString().Split(',').ToList())
+                                    {
+                                        Center.Add(int.Parse(item));
+                                    }
+                                    _center_x = Center[0];
+                                    _center_y = Center[1];
+                                }
                             }
                         }
-                        FrameList.Add(new Sonic3AIRFrame(_name, _file, (int)_rect.X, (int)_rect.Y, (int)_rect.Width, (int)_rect.Height));
+                        if (_center_x != null && _center_y != null) FrameList.Add(new Sonic3AIRFrame(_name, _file, (int)_rect.X, (int)_rect.Y, (int)_rect.Width, (int)_rect.Height, (int)_center_x, (int)_center_y));
+                        else FrameList.Add(new Sonic3AIRFrame(_name, _file, (int)_rect.X, (int)_rect.Y, (int)_rect.Width, (int)_rect.Height));
                     }
+ 
                 }
             }
 
-            public void Save()
+            public Sonic3AIRAnim(string _directory, string _fileLocation)
+            {
+                Directory = _directory;
+                FileLocation = _fileLocation;
+                FrameList.Add(new Sonic3AIRFrame());
+            }
+
+            public void Save(string SaveAsLocation = "")
             {
                 string bc = "}";
                 string bo = "{";
@@ -456,12 +559,18 @@ namespace Sonic_3_A.I.R_Animation_Editor
                 {
                     int index = FrameList.IndexOf(frame);
                     output += nL;
-                    output += $"\t{q}{frame.Name}{q}:  {bo} {q}File{q}: {q}{frame.File}{q}, {q}Rect{q}: {q}{frame.X},{frame.Y},{frame.Width},{frame.Height}{q} {bc}";
+                    output += $"\t{q}{frame.Name}{q}:  {bo} {q}File{q}: {q}{frame.File}{q}, {q}Rect{q}: {q}{frame.X},{frame.Y},{frame.Width},{frame.Height}{q}, {q}Center{q}: {q}{frame.CenterX},{frame.CenterY}{q} {bc}";
                     if (index != count) output += ",";
                 }
                 output += nL;
                 output += "}";
-                File.WriteAllText(FileLocation, output);
+                if (SaveAsLocation != "")
+                {
+                    File.WriteAllText(SaveAsLocation, output);
+                    FileLocation = SaveAsLocation;
+                }
+                else File.WriteAllText(FileLocation, output);
+
             }
 
             public class Sonic3AIRFrame
@@ -472,13 +581,15 @@ namespace Sonic_3_A.I.R_Animation_Editor
                 public int Y;
                 public int Width;
                 public int Height;
+                public int CenterX;
+                public int CenterY;
 
                 public override string ToString()
                 {
                     return Name;
                 }
 
-                public Sonic3AIRFrame(string _name, string _file, int _x, int _y, int _width, int _height)
+                public Sonic3AIRFrame(string _name, string _file, int _x, int _y, int _width, int _height, int _centerX = 0, int _centerY = 0)
                 {
                     Name = _name;
                     File = _file;
@@ -486,9 +597,23 @@ namespace Sonic_3_A.I.R_Animation_Editor
                     Y = _y;
                     Width = _width;
                     Height = _height;
+                    CenterX = _centerX;
+                    CenterY = _centerY;
                 }
 
-                
+                public Sonic3AIRFrame()
+                {
+                    Name = "New Frame";
+                    File = "";
+                    X = 0;
+                    Y = 0;
+                    Width = 0;
+                    Height = 0;
+                    CenterX = 0;
+                    CenterY = 0;
+                }
+
+
             }
         }
 
