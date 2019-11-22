8d491971c1d20a3d0d466ced6f8c4a97ee70dd9c
CarJem Generations <Cwallacecs@Gmail.com>
2019-11-22 09:41:06 -0500

    Added Information for Installer Building

---
+++ Installer/MakeInstaller.bat
@@ -0,0 +1,4 @@
+SET scriptLocation="D:\Users\CarJem\source\sonic3air_repos\Sonic 3 A.I.R Animation Editor\Installer\Sonic 3 A.I.R. Animation Editor.nsi"
+SET makeNISIWLocation="C:\Program Files (x86)\NSIS\makensisw.exe"
+
+%makeNISIWLocation% /V1 %scriptLocation%
