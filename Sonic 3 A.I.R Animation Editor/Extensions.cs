db3e0b5afc8c5fab8cefbaff005c49797b854780
CarJem Generations <Cwallacecs@Gmail.com>
2019-11-22 08:40:30 -0500

    Make Sure the Everything is Staged By Repushing Everything (Part 2)

---
+++ Sonic 3 A.I.R Animation Editor/Extensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Sonic_3_AIR_Animation_Editor
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+        public static bool IsValidURI(string uri)
+        {
+            if (!Uri.IsWellFormedUriString(uri, UriKind.Absolute))
+                return false;
+            Uri tmp;
+            if (!Uri.TryCreate(uri, UriKind.Absolute, out tmp))
+                return false;
+            return tmp.Scheme == Uri.UriSchemeHttp || tmp.Scheme == Uri.UriSchemeHttps;
+        }
+        public static void Move<T>(this List<T> list, int oldIndex, int newIndex)
+        {
+
+            // exit if possitions are equal or outside array
+            if ((oldIndex == newIndex) || (0 > oldIndex) || (oldIndex >= list.Count) || (0 > newIndex) ||
+                (newIndex >= list.Count)) return;
+            // local variables
+            var i = 0;
+            T tmp = list[oldIndex];
+            // move element down and shift other elements up
+            if (oldIndex < newIndex)
+            {
+                for (i = oldIndex; i < newIndex; i++)
+                {
+                    list[i] = list[i + 1];
+                }
+            }
+            // move element up and shift other elements down
+            else
+            {
+                for (i = oldIndex; i > newIndex; i--)
+                {
+                    list[i] = list[i - 1];
+                }
+            }
+            // put element from position 1 to destination
+            list[newIndex] = tmp;
+        }
+    }
+}
